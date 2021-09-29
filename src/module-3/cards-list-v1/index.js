223720c14ad3c62dc14a3ccb5e2a8f0d8356eb97
Nataliya Bernatska <62563765+Natali-Bern@users.noreply.github.com>
2021-09-29 21:39:46 +0300

    Add module 3

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,9 +1,43 @@
-import Card from '../../module-2/card/index.js';
+// import Card from '../../module-2/card/index.js';
 
 export default class CardsList {
-  constructor (data = []) {
-    this.data = data;
+  element;
 
     // ... your logic
+
+    constructor({ data = [], Component = {} }) {
+      this.data = data;
+      this.Component = Component;
+  
+      this.render();
+    }
+
+    getTemplate() {
+      return `<div class="product-list">
+         ${this.data.map(d => new this.Component(d).element.innerHTML).join('')}
+         </div>`
+       }
+
+    render() {
+      const pageWrapper = document.createElement('div');
+        
+      pageWrapper.innerHTML = this.getTemplate();
+      this.element = pageWrapper;
+      }
+
+    update(arr) {
+      this.data = arr;
+      this.render();
+    }
+
+    remove() {
+     if (this.element) {
+     this.element.remove();
+      }
+    }
+
+    destroy() {
+      this.remove();
+     this.element = null;
   }
 }
