28cd8f1ca9128f5f3a5a2e532f6ba70630c51e39
Nataliya Bernatska <62563765+Natali-Bern@users.noreply.github.com>
2021-09-30 20:18:48 +0300

    modified index.js file

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,43 +1,69 @@
-// import Card from '../../module-2/card/index.js';
+// import Card from './../../module-2/card/index.js';
 
 export default class CardsList {
   element;
 
-    // ... your logic
+  constructor({ data = [], Component = {} }) {
+    this.data = data;
+    this.Component = Component;
 
-    constructor({ data = [], Component = {} }) {
-      this.data = data;
-      this.Component = Component;
-  
-      this.render();
-    }
-
-    getTemplate() {
-      return `<div class="product-list">
-         ${this.data.map(d => new this.Component(d).element.innerHTML).join('')}
-         </div>`
-       }
-
-    render() {
-      const pageWrapper = document.createElement('div');
-        
-      pageWrapper.innerHTML = this.getTemplate();
-      this.element = pageWrapper;
-      }
-
-    update(arr) {
-      this.data = arr;
-      this.render();
-    }
-
-    remove() {
-     if (this.element) {
-     this.element.remove();
-      }
-    }
-
-    destroy() {
-      this.remove();
-     this.element = null;
+    this.render();
+    this.getSubElements();
+    this.update(this.data);
   }
+
+  getTemplate() {
+    return `<div>
+      <h1>This is CardList component</h1>
+      <div class="os-product-list" data-element="body"></div>
+    </div>`;
+  }
+
+  render () {
+    const wrapper = document.createElement ('div');
+
+    wrapper.innerHTML = this.getTemplate;
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    this.subElements = result;
+  }
+
+  update (data = []) {
+    this.data = data;
+
+    const cards = data.map(item => {
+      return new this.Component(item).element;
+    });
+
+    if (cards.length) {
+      this.subElements.body.replaceChildren(...cards);
+    } else {
+      this.subElements.body.innerText = 'No products found';
+    }
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
 }
